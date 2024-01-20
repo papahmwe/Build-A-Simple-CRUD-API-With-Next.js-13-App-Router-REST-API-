da4013a14c41c4e09746a85bcae734e8d794e73f
papahmwe <doublehmwenetmsi123@gmail.com>
2024-01-20 13:53:52 +0630

    Build A Simple CRUD API With Next.js 13 App Router (REST API)

---
+++ app/api/blog/route.ts
@@ -0,0 +1,24 @@
+import { addPost, getPosts } from "@/lib/data";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request, res: Response) => {
+  try {
+    const posts = getPosts();
+
+    return NextResponse.json({ message: "OK", posts }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+};
+
+export const POST = async (req: Request, res: Response) => {
+  const { title, desc } = await req.json();
+
+  try {
+    const post = { title, desc, date: new Date(), id: Date.now().toString() };
+    addPost(post);
+    return NextResponse.json({ message: "OK", post }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+};
